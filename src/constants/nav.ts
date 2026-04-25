89dab9557acd9fce2702292c17595d35ff323652
Rody Huancas <rodyhuancas.04@gmail.com>
2026-04-25 00:35:16 -0500

    refactor(constants): remove label field from HEADER_NAV

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -1,20 +1,16 @@
 export type HeaderNavItem = {
-  label    : string;
   href     : string;
   external?: boolean;
 };
 
 export const HEADER_NAV: HeaderNavItem[] = [
   {
-    label: "Documentacion",
     href : "/docs/introduction"
   },
   {
-    label: "Ejemplos",
     href : "/docs/live-examples"
   },
   {
-    label   : "GitHub",
     href    : "https://github.com/rody-huancas/react-rescuer",
     external: true,
   },
