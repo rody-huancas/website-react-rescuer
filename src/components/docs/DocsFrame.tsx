b3753eb24460675aa3668fb1d6a570a0ab33820b
Rody Huancas <rodyhuancas.04@gmail.com>
2026-04-25 23:26:29 -0500

    fix: update DocsFrame layout

--- src/components/docs/DocsFrame.tsx
+++ src/components/docs/DocsFrame.tsx
@@ -5,10 +5,10 @@ const DocsFrame = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative">
       <div className="pt-36">
-        <Container className="relative">
+        <Container className="relative flex items-start">
           <Sidebar />
-
-          <div className="min-w-0 pr-1 lg:pl-76">
+          
+          <div className="min-w-0 pr-1 lg:pl-8 w-full">
             {children}
             <div className="h-16" />
           </div>
